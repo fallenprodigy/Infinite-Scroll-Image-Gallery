0bb3c5e15d5600d67d4fbcac749dcf211e684d18
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-03-18 13:56:04 +0400

    done

--- src/App.js
+++ src/App.js
@@ -34,28 +34,38 @@ const WrapperImage = styled.section`
 
 function App() {
   const [images, setImages] = useState([]);
-  const accessKey = process.env.REACT_APP_ACCESSKEY;
 
   useEffect(() => {
+    fetchImages();
+  }, []);
+
+  const fetchImages = () => {
     const apiRoot = "https://api.unsplash.com";
+    const accessKey = process.env.REACT_APP_ACCESSKEY;
 
     axios
       .get(`${apiRoot}/photos/random?client_id=${accessKey}&count=10`)
       .then((res) => {
         setImages([...images, ...res.data]);
       });
-  }, []);
+  };
+
   return (
     <div className="App">
       <Heading />
       <GlobalStyle />
-      <Loader />
-      <WrapperImage>
-        {images.map((image) => (
-          <UnsplashImage url={image.urls.thumb} key={image.id} />
-        ))}
-      </WrapperImage>
-      <UnsplashImage />
+      <InfiniteScroll
+        dataLength={images.length}
+        next={fetchImages}
+        hasMore={true}
+        loader={<Loader />}
+      >
+        <WrapperImage>
+          {images.map((image) => (
+            <UnsplashImage url={image.urls.thumb} key={image.id} />
+          ))}
+        </WrapperImage>
+      </InfiniteScroll>
     </div>
   );
 }
